82687e83d3dcb7691e13503069ae83772ad769a2
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2023-12-05 18:22:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ with st.form(key='my_form'):
                 border: 1px solid #dddddd;
                 font-size: 16px;
                 width: 650px;
-                height: 200px;
+                height: 40px;
                 /* Add any additional styling here */
             }
             
