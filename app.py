8f277d461a803a8554ee2655a059ecc4ad91c52d
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2023-12-05 15:25:19 +0000

    Upload 2 files

---
+++ app.py
@@ -0,0 +1,40 @@
+## Doctor AI
+import streamlit as st
+
+from langchain.schema import HumanMessage,SystemMessage,AIMessage
+from langchain.chat_models import ChatOpenAI
+
+## Streamlit UI
+st.set_page_config(page_title="Conversational Q&A Chatbot")
+st.header("Hey, Let's Chat")
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+chat=ChatOpenAI(temperature=0.5)
+
+if 'flowmessages' not in st.session_state:
+    st.session_state['flowmessages'] = [
+        SystemMessage(content="Your are an AI Doctor assistant. A user will give an input of what he is suffering from or what health problem he has, you should suggest the user with correct medicine and tell the user how to recover fastly from it. Gve a short and sharp answer. If the input is different from a body or health issue or any other medical issues, tell the user who you are and ask the user to provide the appropriate input.")
+    ]
+
+## Function to load OpenAI model and get respones
+
+def get_chatmodel_response(question):
+
+    st.session_state['flowmessages'].append(HumanMessage(content=question))
+    answer=chat(st.session_state['flowmessages'])
+    st.session_state['flowmessages'].append(AIMessage(content=answer.content))
+    return answer.content
+
+input=st.text_input("Input: ",key="input")
+response=get_chatmodel_response(input)
+
+submit=st.button("Ask the question")
+
+## If ask button is clicked
+
+if submit:
+    st.subheader("The Response is")
+    st.write(response)
