8027f5b0c6c9686aed05b6d4a822a59561afd6bd
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2023-12-05 15:28:42 +0000

    Updated prompt template.
    Updated prompt template to a Doctor AI.

--- app.py
+++ app.py
@@ -1,40 +1,59 @@
-## Doctor AI
 import streamlit as st
-
-from langchain.schema import HumanMessage,SystemMessage,AIMessage
+import time
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
 from langchain.chat_models import ChatOpenAI
 
-## Streamlit UI
-st.set_page_config(page_title="Conversational Q&A Chatbot")
-st.header("Hey, Let's Chat")
+
+def get_chatmodel_response(question):
+    # Retry logic
+    max_retries = 3
+    retries = 0
+
+    while retries < max_retries:
+        try:
+            st.session_state['flowmessages'].append(HumanMessage(content=question))
+            answer = chat(st.session_state['flowmessages'])
+            st.session_state['flowmessages'].append(AIMessage(content=answer.content))
+            return answer.content
+        except Exception as e:
+            print(f"Error: {e}")
+            if "Rate limit" in str(e):
+                print(f"Rate limit exceeded. Waiting and retrying...")
+                time.sleep(5)  # Adjust the waiting time as needed
+                retries += 1
+            else:
+                print("Unhandled exception. Please try again later.")
+                break
+
+    print("Exceeded the maximum number of retries. Please try again later.")
+    return None
+
+# Streamlit app setup
+st.set_page_config(page_title="Doctor AI")
+st.header("Hello, how can I help you?!")
 
 from dotenv import load_dotenv
 load_dotenv()
 import os
 
-chat=ChatOpenAI(temperature=0.5)
+# ChatOpenAI class
+chat = ChatOpenAI(temperature=0.5)
 
 if 'flowmessages' not in st.session_state:
     st.session_state['flowmessages'] = [
         SystemMessage(content="Your are an AI Doctor assistant. A user will give an input of what he is suffering from or what health problem he has, you should suggest the user with correct medicine and tell the user how to recover fastly from it. Gve a short and sharp answer. If the input is different from a body or health issue or any other medical issues, tell the user who you are and ask the user to provide the appropriate input.")
     ]
 
-## Function to load OpenAI model and get respones
+# Streamlit UI
+input_question = st.text_input("Type here.", key="input")
+submit = st.button("Submit")
 
-def get_chatmodel_response(question):
-
-    st.session_state['flowmessages'].append(HumanMessage(content=question))
-    answer=chat(st.session_state['flowmessages'])
-    st.session_state['flowmessages'].append(AIMessage(content=answer.content))
-    return answer.content
-
-input=st.text_input("Input: ",key="input")
-response=get_chatmodel_response(input)
-
-submit=st.button("Ask the question")
-
-## If ask button is clicked
-
+# If the "Ask" button is clicked
 if submit:
-    st.subheader("The Response is")
-    st.write(response)
+    response = get_chatmodel_response(input_question)
+
+    if response is not None:
+        # st.subheader("Here you go,")
+        st.write(response)
+    else:
+        st.subheader("Error: Unable to get response. Please try again later.")
