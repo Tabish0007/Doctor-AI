1ada90fbd45f5b1ba68eb62ba6cdd9933989d362
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2023-12-05 17:38:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -72,9 +72,9 @@ st.markdown(
         .stTextInput {
             border-radius: 15px;
             padding: 12px;
-            margin-top: 60px;
+            margin-top: 10px;
             margin-bottom: 10px;
-            box-shadow: 2px 2px 7px #888888;
+            box-shadow: 7px 7px 7px #888888;
             border: 1px solid #dddddd;
             font-size: 16px;
             width: 700px;
