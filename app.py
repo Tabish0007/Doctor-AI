99f7c6b55475b226528a0c92ae6077196e8e5dc6
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2023-12-11 12:52:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -89,36 +89,36 @@ if 'flowmessages' not in st.session_state:
 
 
 # Streamlit UI
-with st.form(key='my_form', clear_on_submit=True):
-    st.markdown(
-        """
-        <style>
-            .stTextInput {
-                border-radius: 15px;
-                padding: 12px;
-                margin-top: 10px;
-                margin-bottom: 10px;
-                box-shadow: 2px 2px 5px #888888;
-                border: 1px solid #dddddd;
-                font-size: 16px;
-                width: 100%;
-                height: 100px;
-            }
-            .blue-text {
-                color: blue;
-            }
-            .black-text {
-                color: black;
-            }
-            .separator {
-                border-top: 2px solid #888888;
-                margin-top: 10px;
-                margin-bottom: 10px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+st.markdown(
+    """
+    <style>
+        .stTextInput {
+            border-radius: 15px;
+            padding: 12px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+            box-shadow: 2px 2px 5px #888888;
+            border: 1px solid #dddddd;
+            font-size: 16px;
+            width: 100%;
+            height: 100px;
+        }
+        .blue-text {
+            color: blue;
+        }
+        .black-text {
+            color: black;
+        }
+        .separator {
+            border-top: 2px solid #888888;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Display conversation history
 for message in reversed(st.session_state['flowmessages']):
     if isinstance(message, AIMessage):
