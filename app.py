1a366807e54f43c79d1d73b15d2a1ddf1324f1fc
Sailesh S <SaileshSivan@users.noreply.huggingface.co>
2024-01-12 10:02:25 +0000

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_chatmodel_response(question):
 
 # Streamlit app setup
 st.set_page_config(page_title="Doctor AI", page_icon="💊", layout="wide", initial_sidebar_state="collapsed")
-st.snow()
+# st.snow()
 
 st.header("Hello, I am Doctor AI. How can I help you?")
 
